c0f2850d1787c36fdd4692b552e54217061d0dfb
amh5366 <amh5366@upenn.edu>
2018-06-29 10:36:45 -0400

    added regex to remove junk items from steam object

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 var content = [];
 
-for (let i = 0; i < steamApps.applist.apps.length; i++){ 
-    content.push({ 
-        "appid": steamApps.applist.apps[i].appid,
-        "name":steamApps.applist.apps[i].name.replace(/:/g,'')
-    }) 
+for (let i = 0; i < steamApps.applist.apps.length; i++){
+    var deleteObj = /\b(beta|trailer|movie|demo|teaser|server|DLC|map\spack|multiplayer\sskins|weapons\spack|weapons\sskins|starter\spack|premium\spack|pre.*order|sdk|player\sprofiles|Workshop|creation\sclub|texture\spack)\b/gi;
+    if (deleteObj.test(steamApps.applist.apps[i].name)) {
+        delete steamApps.applist.apps[i];
+    } else {
+        content.push({
+            "appid": steamApps.applist.apps[i].appid,
+            "name":steamApps.applist.apps[i].name.replace(/[-:]/g,' ')
+        })
+    }
 }
 
 
@@ -12,14 +17,21 @@ for (let i = 0; i < steamApps.applist.apps.length; i++){
     console.log(content);
 function gameName (game) {
     var gameSearch = game.split(' ').join(".*");
+    var gameSearchFull = '\\b^'+ gameSearch +'$\\b';
     console.log(gameSearch);
     var search = new RegExp(gameSearch, 'i');
+    var search1 = new RegExp(gameSearchFull, 'i');
     console.log(search);
     for (let i = 0; i < content.length; i++) {
         if(content[i].name.match(search)) {
             console.log('Name:' + content[i].name + "\n" + "Appid: " + content[i].appid);
         }
     }
+    for (let i = 0; i < content.length; i++) {
+        if(content[i].name.match(search1)) {
+            console.log('Name BEST MATCH: ' + content[i].name + "\n" + "Appid: " + content[i].appid);
+        }
+    }
 }
 
-gameName("Star Wars the Old Republic");
+gameName("Fallout 4");
